c3d22a9124e47a2070991476a40729a0dd8671a0
Laurel Hart <laurel.elise.hart@gmail.com>
2013-11-29 22:10:46 -0800

    Major restructuring; mind the dust

---
+++ ircPromptBot.py
@@ -0,0 +1,315 @@
+#!/usr/bin/python2.7
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol
+from twisted.internet.task import LoopingCall, deferLater
+from collections import defaultdict
+from random import shuffle
+import sys, re
+from datetime import *
+import listbot
+
+class ircPromptBot(irc.IRCClient):
+    """A bot that gives and takes prompts on request."""
+    """Strongly based on ircLogBot example by Twisted Matrix Laboratories."""
+    nickname = "promptbot"
+
+    def __init__(self):
+        self.lists = ["prompt", "advice", "praise"]
+        self.lastLists = defaultdict(lambda: "prompt")
+        self.promptbot = listbot.ListBot()
+        for l in self.lists:
+            self.promptbot.addList(l)
+    
+    def connectionMade(self):
+        irc.IRCClient.connectionMade(self)
+        self.promptbot.loadEntries("prompt", self.factory.infile)
+    
+    def connectionLost(self, reason):
+        if self.promptbot:
+            self.promptbot.backupAll(self.factory.outfile)
+        irc.IRCClient.connectionLost(self, reason)
+
+    def signedOn(self):
+        self.join(self.factory.channel)
+
+    def joined(self, channel):
+        #if t+, call startTopic()
+        pass
+
+    def setTopic(self, channel):
+        today = datetime.today()
+        day = today.weekday()
+        topic = ""
+        if today.month == 10: 
+            topic = "%d days until NaNoWriMo " % (31 - today.day)
+        elif today.month == 11:
+            topic = "Do you have your %d words yet? " % (int(float(50000)/30*today.day))
+        if day == 2:
+            if not topic == "":
+                topic += "| "
+            topic += "Worldbuilding Wednesday: "
+            prompt = self.promptbot.entryByTag("prompt","worldbuilding",channel)
+            topic += prompt
+        elif day == 0:
+            if not topic == "":
+                topic += "| "
+            topic += "Character Monday: "
+            prompt = self.promptbot.entryByTag("prompt","character",channel)
+            topic += prompt
+        elif day == 3:
+            if not topic == "":
+                topic += "| "
+            topic += "Theme Thursday: "
+            prompt = self.promptbot.entryByTag("prompt","theme",channel)
+            topic += prompt
+        self.topic(channel, topic)
+
+    def startTopic(self, channel):
+        #will try to set the topic every day at midnight until stopTopic is called.
+        self.lc = LoopingCall(self.setTopic, channel)
+        delay = self.secondsToMidnight()
+        d = deferLater(reactor, 1, self.lc.start, 30)
+        #d = deferLater(reactor, delay, self.lc.start, 86400)
+        #figure out time til midnight for delay, then should loop every 24 hours (86400 seconds).
+        self.d = d
+        d.addCallback(self.startTopic)
+
+    def secondsToMidnight(self):
+        nextMidnight = datetime.combine(date.today() + timedelta(1), time(0,1))
+        return (nextMidnight - datetime.now()).seconds
+
+    def stopTopic(self):
+        pass
+    
+    def privmsg(self, user, channel, msg):
+        user = user.split('!', 1)[0]
+        if channel == self.nickname:
+            channel = user
+        if msg.startswith(self.nickname) or channel == user:
+            pattern = self.nickname + "\W( )?"
+            msg = re.sub(pattern, '', msg)
+            if "help" in msg:
+                self.helpMenu(msg, user, channel)
+            elif msg == "github":
+                msg = "%s: %s" % (user, "https://github.com/konayashi/promptbot")
+                self.msg(channel, msg)
+            else:
+                #check for command
+                self.commands(msg, user, channel)
+    
+    def commands(self, msg, user, target):
+        m = msg.split()
+        for l in self.lists: 
+            if len(m) >= 2 and l in m[:2]:
+                self.lastLists[target] = l 
+                if msg.startswith("add "+l):
+                    msg = re.sub("add "+l+"\W( )?", '', msg)
+                    #add rest of msg to list
+                    self.promptbot.addEntry(l, msg, target)
+                    msg = (l+" added.").capitalize()
+                    self.msg(target, msg) 
+                    return
+                elif msg.startswith("last "+l):
+                    msg = self.promptbot.last(l, target)
+                    self.msg(target, msg)
+                    return
+                elif msg.startswith(l+" tags"):
+                    self.msg(target, self.promptbot.listTags(l))
+                    return
+                elif msg.startswith("backup "+l):
+                    out = self.promptbot.backup(l, self.factory.outfile)
+                    msg = "%d %s entries backed up to %s." % (len(self.promptbot.lists[l].entries), l, out)
+                    target = user
+                    self.msg(target, msg)
+                    return
+                elif msg.startswith("load "+l):
+                    msg = re.sub("load "+l+"(s)?(\s)?", '', msg)
+                    msg = self.promptbot.loadEntries(l, msg)
+                    self.msg(target, msg)
+                    return
+                msg = re.sub(l,'', msg)
+                tags = re.findall("\((.+)\)", msg)
+                tags.extend(re.findall("([^\(\s]+)", msg))
+                if tags:
+                    self.lastLists[target] = "prompt"
+                    i = 0
+                    shuffle(tags)
+                    prompt = "No entries"
+                    while "No entries" in prompt and i < len(tags):
+                        prompt = self.promptbot.entryByTag(self.lastLists[target], tags[i], target)
+                        i += 1
+                    if not "No entries" in prompt:
+                        msg = "%s: %s: %s" % (user, tags[i-1].capitalize(), prompt)
+                        self.msg(target, msg)
+                        return
+                index = re.findall('[0-9]+', msg)
+                if index:
+                    msg = "%s: %s" % (user, self.promptbot.entryByIndex(l, int(index[0]),target))
+                    self.msg(target, msg)
+                    return
+                msg = "%s: %s" % (user, self.promptbot.randomEntry(l, target)) 
+                self.msg(target, msg)
+                return
+        else: 
+            if msg.startswith("last"):
+                msg = self.promptbot.last(self.lastLists[target], target)
+                self.msg(target, msg)
+                return
+            elif msg.startswith("add tag"):
+            #add a tag to last entry printed
+                tags = re.findall("\((.+)\)", msg)
+                tags.extend(re.findall("([^\(\s]+)", msg))
+                if tags:
+                    self.promptbot.addTags(self.lastLists[target], tags, target)
+                    if len(tags) > 1:
+                        self.msg(target, "Tags added.")
+                    else:
+                        self.msg(target, "Tag added.")
+                else:
+                    self.msg(target, "No tags found. Try 'add tag(s) #tag or #(tag)'.")
+                return
+            elif msg.startswith("add source"):
+            #add a source to last entry printed
+                source = re.findall("@\((.+)\)", msg)
+                source.extend(re.findall("@([^\(\s]+)", msg))
+                if source:
+                    self.promptbot.addSource(source, target)
+                    if len(source) > 1:
+                        self.msg(target, "Sources added.")
+                    else:
+                        self.msg(target, "Source added.")
+                else:
+                    self.msg(target, "No source found. Try 'add source(s) @source or @(source with spaces)'.")        
+                return
+            elif msg.startswith("tags?"):
+            #get all tags for last entry printed
+                self.msg(target, self.promptbot.getTags(self.lastLists[target], target)) 
+                return
+            elif msg.startswith("source?"):
+            #get all sources for last entry printed
+                self.msg(target, self.promptbot.getSource(self.lastLists[target], target)) 
+                return
+            elif msg.startswith("index?"):
+            #get index for last entry printed
+                self.msg(target, self.promptbot.getIndex(self.lastLists[target],target)) 
+                return 
+            elif msg.startswith("remove tag"):
+            #remove a tag from last entry printed
+                tags = re.findall("#\((.+)\)", msg)
+                tags.extend(re.findall("#([^\(\s]+)", msg))
+                if tags:
+                    self.promptbot.removeTags(self.lastLists[target], tags, target)
+                    if len(tags) > 1:
+                        self.msg(target, "Tags removed.")
+                    else:
+                        self.msg(target, "Tag removed.")
+                else:
+                    self.msg(target, "No tags found. Try 'remove tag(s) #tag or #(tag with spaces)'.")
+                return
+            elif msg.startswith("remove source"):
+            #remove a source from last entry printed
+                source = re.findall("@\((.+)\)", msg)
+                source.extend(re.findall("@([^\(\s]+)", msg))
+                if source:
+                    self.promptbot.removeSource(source, target)
+                    if len(source) > 1:
+                        self.msg(target, "Sources removed.")
+                    else:
+                        self.msg(target, "Source removed.")
+                else:
+                    self.msg(target, "No source found. Try 'remove source(s) @source or @(source with spaces)'.")        
+                return
+            elif msg.startswith("backup"):
+                out = self.promptbot.backupAll(self.factory.outfile)
+                msg = "All lists backed up."
+                target = user
+                self.msg(target, msg)
+                return
+            elif msg.startswith("add list"):
+            #add a new list
+                msg = re.sub("add list(\s)?", '', msg)
+                self.promptbot.addList(msg)
+                self.lists.append(msg)
+                self.msg(target, "New %s list added." % (msg))
+                return
+            elif msg.startswith("tags"):
+                msg = self.promptbot.listAllTags()
+                self.msg(target, msg)
+                return
+            if msg.startswith("start topic"):
+                self.startTopic(target)
+                self.msg(target, "I will try to set the topic every night at midnight.")
+                return
+            #if msg.startswith("stop topic"):
+                #self.stopTopic()
+                #self.msg(target, "I will stop trying to set the topic.")
+                #return
+            if msg.startswith("topic"):
+                self.setTopic(target)
+                return
+            tags = re.findall("#\((.+)\)", msg)
+            tags.extend(re.findall("#([^\(\s]+)", msg))
+            if tags:
+                self.lastLists[target] = "prompt"
+                i = 0
+                shuffle(tags)
+                prompt = "No entries"
+                while "No entries" in prompt and i < len(tags):
+                    prompt = self.promptbot.entryByTag(self.lastLists[target], tags[i], target)
+                    i += 1
+                if not "No entries" in prompt:
+                    msg = "%s: %s: %s" % (user, tags[i-1].capitalize(), prompt)
+                    self.msg(target, msg)
+                    return
+            index = re.findall('[0-9]+', msg)
+            if index:
+                msg = "%s: %s" % (user, self.promptbot.entryByIndex(self.lastlists[target], int(index[0]),target))
+                self.msg(target, msg)
+                return
+            elif "random" in msg:
+                msg = "%s: %s" % (user, self.promptbot.completelyRandomEntry(target))
+                self.msg(target, msg)
+                return 
+            else: 
+                msg = "Eh? Try asking me for a prompt."
+                self.msg(target, msg)
+                return
+
+    def helpMenu(self, msg, user, channel):
+        if "help prompts" in msg:
+            self.msg(channel, "'#(tag)' for a prompt with that tag. Multiple tags will give a prompt with a tag randomly selected from those provided.\n\t'promptbot, I want a prompt from #worldbuilding or #theme' or 'promptbot, #worldbuilding #theme'\nUsing a number when talking to promptbot will return the prompt with that index. The last prompt's index can be retrieved with 'index?'\nFor a random prompt, just use the word 'prompt'\n\t'promptbot, gimme a prompt' or 'promptbot, prompt'\n'last' will reprint the last prompt.")
+        elif "help tags" in msg:
+            self.msg(channel, "'tags?' will give the list of tags for the last given prompt.\n'add tag(s) #tag #(tag with spaces)' will add those tags to the last given prompt.")
+        elif "help sources" in msg:
+            self.msg(channel, "'source?' will give the source for the last given prompt.\n'add source @(source)' will add that source to the last given prompt.")
+        else:
+            self.msg(channel,"Help topics include: 'prompts', 'tags', 'sources' \nType 'help $TOPIC' for more info.\nView promptbot's code at https://github.com/konayashi/promptbot")
+
+class BotFactory(protocol.ClientFactory):
+    def __init__(self, channel, files):
+        self.channel = channel
+        self.infile = files[0]
+        if len(files) > 1:
+            self.outfile = files[1]
+        else:
+            self.outfile = self.infile + ".pb"
+
+    def buildProtocol(self, addr):
+        p = ircPromptBot()
+        p.factory = self
+        return p
+
+    def clientConnectionLost(self, connector, reason):
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "connection failed:", reason
+        reactor.stop()
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        f = BotFactory(sys.argv[2], sys.argv[3:])
+        reactor.connectTCP(sys.argv[1], 6667, f)
+        reactor.run()
+    else:
+        print "Usage: ircBot host channel <input prompt file> [prompt output file]"
---
+++ listbot.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python3.7
+from collections import defaultdict
+from random import choice
+import sys, re
+
+class Entry(object):
+    def __init__(self, text, tags, source):
+        self.text = text
+        self.tags = tags
+        self.source = source
+
+class List(object):
+    def __init__(self):
+        self.entries = list()
+        self.tags = {}
+
+class ListBot:
+    def __init__(self):
+        self.indices = defaultdict(lambda: defaultdict(int))
+        #Keeps track of the last element accessed per list per channel by
+        #using the channel name as a key.
+        self.lists = {}
+
+    def addList(self, listName):
+        if listName in self.lists:
+            return "There is already a list with that name."
+        else:
+            self.lists[listName] = List()
+            for channel in self.indices:
+                self.indices[channel][listName] = 0
+            return "List %s added." % listName
+
+    def addEntry(self, listName, entryText, channel, dupCheck=False):
+        tags = set(re.findall("#\(([^\)]+)\)", entryText))
+        tags.update(re.findall("#([^\(\s]+)", entryText))
+        source = re.findall("@\(([^\)]+)\)", entryText)
+        entryText = (re.sub("#\(([^\)]+)\)", "", entryText)).strip()
+        entryText = (re.sub("#([^\(\s]+)", "", entryText)).strip()
+        entryText = (re.sub("@\((.*)\)", "", entryText)).strip()
+        if dupCheck:
+        #duplicate check
+            for i in range(0, len(self.lists[listName].entries)):
+                for entry in self.lists[listName].entries:
+                    if entryText == entry.text:
+                        return
+        newEntry = Entry(entryText, tags, source)
+        self.lists[listName].entries.append(newEntry)
+        index = self.lists[listName].entries.index(newEntry)
+        for tag in tags:
+            if tag in self.lists[listName].tags:
+                self.lists[listName].tags[tag].append(index)
+            else:
+                self.lists[listName].tags[tag] = [index]
+        self.indices[channel][listName] = index
+   
+    def loadEntries(self, listName, inf):
+        try:
+            with open(inf, "r"):
+                i = 0
+                for line in open(inf, "r").readlines():
+                    self.addEntry(listName, line, "listbot", True)
+                    i += 1
+                open(inf, "r").close()
+                return "%d %s entries in %s loaded." % (i, listName, inf)
+        except IOError: 
+            return "No such file '%s'" % (inf)
+
+    def addTags(self, listName, tags, channel, index = ""):
+        if not index:
+            index = self.indices[channel][listName]
+        for tag in tags:
+            if tag in self.lists[listName].tags:
+                self.lists[listName].tags[tag].append(index)
+            else:
+                self.lists[listName].tags[tag] = [index]
+        self.lists[listName].entries[index].tags.update(tags)
+
+    def addSource(self, listName, source, channel, index = ""):
+        if not index:
+            index = self.indices[channel][listName]
+        self.lists[listName].entries[index].source.extend(source)
+
+    def removeTags(self, listName, tags, channel, index = ""):
+        if not index:
+            index = self.indices[channel][listName]
+        for tag in tags:
+            if tag in self.lists[listName].tags:
+                if index in self.lists[listName].tags[tag]:
+                    self.lists[listName].tags[tag].remove(index)
+                    if tag in self.lists[listName].tags:
+                        self.lists[listName].entries[index].tags.remove(tag)
+
+    def removeSource(self, listName, source, channel, index = ""):
+        if not index:
+            index = self.indices[channel][listName]
+        for s in source:
+            if s in self.lists[listName].entries[index].source:
+                self.lists[listName].entries[index].source.remove(s)
+
+    def getTags(self, listName, channel, index = ""):
+        if not index:
+            index = self.indices[channel][listName]
+        if len(self.lists[listName].entries) > 0:
+            tags = '; '.join(self.lists[listName].entries[index].tags)
+            if tags:
+                return tags
+            else:
+                return "No tags."
+        else:
+            return "No %s entries to have tags." % listName
+    
+    def getSource(self, listName, channel, index = ""):
+        if not index:
+            index = self.indices[channel][listName]
+        if len(self.lists[listName].entries) > 0:
+            source = '; '.join(self.lists[listName].entries[index].source)
+            if source:
+                return source
+            else:
+                return "No source given."
+        else:
+            return "No %s entries to have sources." % listName
+
+    def getIndex(self, listName, channel, index = ""):
+        if not index:
+            index = self.indices[channel][listName]
+        if len(self.lists[listName].entries) > 0:
+            return "%s #%d" % (listName.capitalize(), index)
+        else:
+            return "No %s entries to have indexes." % listName
+
+    def last(self, listName, channel):
+        index = self.indices[channel][listName]
+        if len(self.lists[listName].entries) > 0:
+            return self.lists[listName].entries[index].text
+        else:
+            return "%s has no entries." % listName.capitalize()
+
+    def randomEntry(self, listName, channel):
+        l = len(self.lists[listName].entries)
+        if l > 0:
+            index = choice(range(0, len(self.lists[listName].entries)))
+            self.indices[channel][listName] = index
+            return self.lists[listName].entries[index].text
+        else:
+            return "%s has no entries." % listName
+        
+    def completelyRandomEntry(self, channel):
+        lists = []
+        for l in self.lists:
+           if len(self.lists[l].entries) > 0:
+               lists.append(l)
+        if lists:
+            randomList = choice(lists)
+            entry = choice(range(0, len(self.lists[randomList].entries)))
+            self.indices[channel][randomList] = entry
+            return self.lists[randomList].entries[entry].text
+        else:
+            return "there are no entries."
+    
+    def entryByTag(self, listName, tag, channel):
+        if tag in self.lists[listName].tags:
+            index = choice(self.lists[listName].tags[tag])
+            self.indices[channel][listName] = index
+            return self.lists[listName].entries[index].text
+        else:
+            return "No entries have tag %s." % tag
+
+    def entryByIndex(self, listName, index, channel):
+        if index < len(self.lists[listName].entries):
+            self.indices[channel][listName] = index
+            return self.lists[listName].entries[index].text
+        else:
+            return "I only contain %s %s entries." % (len(self.lists[listName].entries),listName)
+    
+    def listTags(self, listName):
+        if len(self.lists[listName].tags) > 0:
+            msg = ', '.join(self.lists[listName].tags.keys())
+        else:
+            msg = "%s has no tags." % (listName.capitalize())
+        return msg
+
+    def listAllTags(self):
+        msg = ""
+        for listName in self.lists:
+            tags = ', '.join(self.lists[listName].tags.keys())
+            tags = listName + ": " + tags + "\n"
+            msg += tags
+        return msg
+
+    def backup(self, listName, outfile):
+        if len(self.lists) == 1:
+            outname = outfile 
+        else:
+            outname = outfile + "." + listName 
+        out = open(outname, "w")
+        for entry in self.lists[listName].entries:
+            line = entry.text
+            for tag in entry.tags:
+                line += " #(" + tag + ")"
+            for source in entry.source:
+                line += " @(" + source + ")"
+            line += "\n"
+            out.write(line)
+        out.close()
+        return outname
+
+    def backupAll(self, outfile):
+        for listName in self.lists:
+            if len(self.lists) == 1:
+                out = open(outfile, "w")
+            else:
+                out = open(outfile + "." + listName, "w")
+            for entry in self.lists[listName].entries:
+                line = entry.text
+                for tag in entry.tags:
+                    line += " #(" + tag + ")"
+                for source in entry.source:
+                    line += " @(" + source + ")"
+                line += "\n"
+                out.write(line)
+            out.close()
+
